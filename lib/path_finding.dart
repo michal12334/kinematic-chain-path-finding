a951e13340cfa881fd3cc75ad4ff05acc6cd43e2
Michal Boguszewski <michal1842001@gmail.com>
2024-12-17 22:51:06 +0100

    Add path finding header

--- lib/path_finding.dart
+++ lib/path_finding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kinematic_chain_path_finding/header_text.dart';
 
 class PathFinding extends StatelessWidget {
   const PathFinding({
@@ -9,10 +10,14 @@ class PathFinding extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
+        const HeaderText('Path Finding'),
+        const SizedBox(
+          height: 10,
+        ),
         FilledButton.tonal(
           onPressed: () {},
           child: const Text('Compute'),
-        )
+        ),
       ],
     );
   }
