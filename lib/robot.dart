f095f941930fba5d8c2ba1c07c6eb4b61afef6ef
Michal Boguszewski <michal1842001@gmail.com>
2024-12-17 20:23:33 +0100

    Fix triangle condition

--- lib/robot.dart
+++ lib/robot.dart
@@ -7,15 +7,11 @@ class Robot {
     required this.x,
     required this.y,
   }) {
-    if (x * x + y * y > (l1 + l2) * (l1 + l2)) {
-      x1 = y1 = x2 = y2 = null;
-      return;
-    }
     if (y == 0) {
       x1 = x2 = (l1 * l1 - l2 * l2 + x * x) / (2 * x);
       y1 = sqrt(l1 * l1 - pow(l1 * l1 - l2 * l2 + x * x, 2) / (4 * x * x));
       y2 = -y1!;
-    } else if (x * x + y * y > 0 && l1 + l2 >= x * x + y * y) {
+    } else if (x * x + y * y > 0 && (l1 + l2) * (l1 + l2) >= x * x + y * y) {
       x1 = 1 /
           (2 * (x * x + y * y)) *
           (-sqrt(
